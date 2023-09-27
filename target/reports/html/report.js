1139b49f7592c2f0d0497d543e3221dfb4e546cf
Thiago Lyon <thiago.lyon.passos@gmail.com>
2023-09-27 10:30:37 -0300

    fix smoke tests

--- target/reports/html/report.js
+++ target/reports/html/report.js
@@ -1,16 +1,24 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/resources/features/trabalhe.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/resources/features/contato.feature");
 formatter.feature({
-  "name": "Cadastrando para vaga de emprego",
-  "description": "    Eu como candidato para vaga\n    Quero me candidatar na vaga de analista de Testes Automatizados\n    Para fazer parte da equipe da SoftExpert",
-  "keyword": "Funcionalidade"
+  "name": "Acessando tela de contato da SoftExpert",
+  "description": "    Eu como possível cliente\n    Quero acessar a tela de contato da SoftExpert\n    Para poder entrar em contato com o setor de vendas",
+  "keyword": "Funcionalidade",
+  "tags": [
+    {
+      "name": "@contato"
+    }
+  ]
 });
 formatter.scenario({
-  "name": "Cadastrando para a vaga de analista de testes automatizados",
+  "name": "Acessando tela de contato do Brasil",
   "description": "",
   "keyword": "Cenário",
   "tags": [
     {
-      "name": "@trabalhe-conosco"
+      "name": "@contato"
+    },
+    {
+      "name": "@contato-brasil"
     }
   ]
 });
@@ -25,71 +33,41 @@ formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "clico no botão Trabalhe Conosco",
+  "name": "clico no botão Contato",
   "keyword": "Quando "
 });
 formatter.match({
-  "location": "TrabalheSteps.clico_no_botão_Trabalhe_Conosco()"
+  "location": "ContatoSteps.clico_no_botão_Contato()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "clico no botão Ver Vagas Abertas",
+  "name": "clico no botão América do Sul",
   "keyword": "E "
 });
 formatter.match({
-  "location": "TrabalheSteps.clico_no_botão_Ver_Vagas_Abertas()"
+  "location": "ContatoSteps.clico_no_botão_América_do_Sul()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "clico no botão Pessoa analista de Testes automatizados",
+  "name": "clico no botão Brasil",
   "keyword": "E "
 });
 formatter.match({
-  "location": "TrabalheSteps.clico_no_botão_Pessoa_analista_de_Testes_automatizados()"
+  "location": "ContatoSteps.clico_no_botão_Brasil()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "clico no botão Apply",
-  "keyword": "E "
-});
-formatter.match({
-  "location": "TrabalheSteps.clico_no_botão_Apply()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "preencho os dados solicitados",
-  "keyword": "E "
-});
-formatter.match({
-  "location": "TrabalheSteps.preencho_os_dados_solicitados()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "clico no botão Submit Application",
-  "keyword": "E "
-});
-formatter.match({
-  "location": "TrabalheSteps.clico_no_botão_Submit_Application()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "a aplicação deve exibir a mensagem de sucesso",
+  "name": "a aplicação deve exibir o contato da SoftExpert no Brasil",
   "keyword": "Então "
 });
 formatter.match({
-  "location": "TrabalheSteps.a_aplicação_deve_exibir_a_mensagem_de_sucesso()"
+  "location": "ContatoSteps.a_aplicação_deve_exibir_o_contato_da_SoftExpert_no_Brasil()"
 });
 formatter.result({
   "status": "passed"
